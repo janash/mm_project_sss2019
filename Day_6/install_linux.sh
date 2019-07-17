57ca90295f036b45b019cb2463a47132b486445a
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-07-16 23:46:02 -0400

    added module relocation

--- Day_6/install_linux.sh
+++ Day_6/install_linux.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+################################
+# A test script for Linux X86_64
+################################
+
 # Download conda installation script
 wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 
@@ -7,5 +13,11 @@ bash Anaconda3-2019.03-Linux-x86_64.sh
 # Initialize conda
 source ~/.bashrc
 
-# Installa packages
+# Install packages
 conda install pybind11 cmake eigen
+
+# Compile C++/python module (mm_cpp.so)
+bash mm_cpp/build.sh
+
+# Move module to current directory
+mv mm_cpp/mm_cpp.so .
